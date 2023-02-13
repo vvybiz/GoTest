db6dda8ebca4d5c9ff65f84d1b58d4ec00df2889
Victor <vvybiz@gmail.com>
2023-02-13 16:20:09 +0200

    add

--- arr.go
+++ arr.go
@@ -1,7 +1,15 @@
-package arr
+package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+
+	todoList := [4]string{
+		"11", "22", "33", "44",
+	}
+
+	for i, item := range todoList {
+		fmt.Printf("%d. %s\n", i+1, item)
+	}
+
 }
