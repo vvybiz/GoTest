37a8fdd41fc1e0bbad13c3cc936e3d46e0ff0418
Victor <vvybiz@gmail.com>
2023-02-13 17:38:41 +0200

    Final

--- arr.go
+++ arr.go
@@ -4,12 +4,28 @@ import "fmt"
 
 func main() {
 
-	todoList := [4]string{
+	todoList := []string{
 		"11", "22", "33", "44",
 	}
 
 	for i, item := range todoList {
 		fmt.Printf("%d. %s\n", i+1, item)
 	}
+
+	for i, item := range reverse(todoList) {
+		fmt.Printf("%d. %s\n", i+1, item)
+	}
+
+}
+
+func reverse(todoList []string) []string {
+
+	var reverse []string
+
+	for i := len(todoList) - 1; i >= 0; i-- {
+		reverse = append(reverse, todoList[i])
+	}
+
+	return reverse
 
 }
